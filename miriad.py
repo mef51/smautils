4ace18a7926d6cf992c197dbb6e2251235e0c064
mef51 <hms.mohammed@gmail.com>
2017-09-12 13:39:10 -0400

    throw on nonzero return code and add a few miriad tasks

--- miriad.py
+++ miriad.py
@@ -7,8 +7,9 @@ def do(command, options={}):
 	for option in options:
 		command += ' '
 		command += '{}={}'.format(option, options[option])
-	print(command)
+	# print(command)
 	result = subprocess.run(command, shell=True)
+	result.check_returncode()
 	return result.returncode
 
 def uvspec(options={}):
@@ -23,11 +24,20 @@ def cgcurs(options={}):
 def uvaver(options={}):
 	return do('uvaver', options)
 
+def uvcat(options={}):
+	return do('uvcat', options)
+
+def uvsort(options={}):
+	return do('uvsort', options)
+
 def selfcal(options={}):
 	return do('selfcal', options)
 
 def gpplt(options={}):
 	return do('gpplt', options)
+
+def gpcopy(options={}):
+	return do('gpcopy', options)
 
 def invert(options={}):
 	return do('invert', options)
@@ -49,6 +59,9 @@ def imstat(options={}):
 
 def maxfit(options={}):
 	return do('maxfit', options)
+
+def maths(options={}):
+	return do('maths', options)
 
 if __name__ == '__main__':
 	uvspec({
