6b64417a720ef7e385bc18ad7ef7d69059179804
mef51 <hms.mohammed@gmail.com>
2018-05-08 21:54:24 -0400

    release

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Tools for the analysis done in Chamma et al. 2018 (submitted) and https://github
 `miriad.py` is a wrapper around the Miriad data reduction package for radio interferometry.
 
 `squint.py` is a package that performs beam offset correction to remove false Stokes V signals
+
+Installation
+====
+`pip install smautils` then `import miriad`. See `squint.py` for an example on usage.
--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup
 
 setup(name='smautils',
-	version='0.1',
-	description='Miriad and Python for great profit',
+	version='1.0',
+	description='Wrapper for the Miriad Interferometric Reduction Package',
 	author='Mohammed Chamma',
 	author_email='mchamma@uwo.ca',
+	url='https://github.com/mef51/smautils',
+	keywords='radio interferometry SMA wrapper miriad',
+	classifiers=["Programming Language :: Python :: 3"],
 	license='MIT')
